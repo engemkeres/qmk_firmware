3989b5f8bda68ee912367dc2c0a297e31c3276d9
engemkeres <100580144+engemkeres@users.noreply.github.com>
2024-12-08 15:44:43 +0100

    Clean up

--- keyboards/vaucasy/keymaps/engemkeres/keymap.c
+++ keyboards/vaucasy/keymaps/engemkeres/keymap.c
@@ -31,27 +31,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *              └───┴───┴───┴───┘         └───┴───┴───┴───┘
      */
     [_BASE] = LAYOUT_4x7(
-        KC_ESC,   HU_0,               KC_1,      KC_2,    KC_3,    KC_4,               KC_5,    KC_6,    KC_7,     KC_8,     KC_9,    HU_ODIA,  HU_UDIA,  HU_OACU,
-        CW_TOGG,  KC_TAB,             KC_Q,      KC_W,    KC_E,    KC_R,               KC_T,    HU_Z,    KC_U,     KC_I,     KC_O,    KC_P,     HU_ODAC,  HU_UACU,
-        KC_LWIN,  KC_LSFT,            KC_A,      KC_S,    KC_D,    LT(_MOUS, KC_F),    KC_G,    KC_H,    KC_J,     KC_K,     KC_L,    HU_EACU,  HU_AACU,  HU_UDAC,
-        KC_LCTL,  LT(_MOUS, HU_IACU), HU_Y,      KC_X,    KC_C,    KC_V,               KC_B,    KC_N,    KC_M,     KC_COMMA, KC_DOT,  HU_MINS,  KC_ALGR,  KC_NO,
-                                      KC_MPLY,   KC_LALT, KC_SPC,  MO(_FUNC),          KC_DEL,  KC_ENT,  KC_BSPC,  KC_CPNL
+        KC_ESC,   HU_0,               KC_1,      KC_2,    KC_3,    KC_4,               KC_5,              KC_6,    KC_7,     KC_8,     KC_9,    HU_ODIA,  HU_UDIA,  HU_OACU,
+        CW_TOGG,  KC_TAB,             KC_Q,      KC_W,    KC_E,    KC_R,               KC_T,              HU_Z,    KC_U,     KC_I,     KC_O,    KC_P,     HU_ODAC,  HU_UACU,
+        KC_LWIN,  KC_LSFT,            KC_A,      KC_S,    KC_D,    LT(_MOUS, KC_F),    KC_G,              KC_H,    KC_J,     KC_K,     KC_L,    HU_EACU,  HU_AACU,  HU_UDAC,
+        KC_LCTL,  LT(_MOUS, HU_IACU), HU_Y,      KC_X,    KC_C,    KC_V,               KC_B,              KC_N,    KC_M,     KC_COMMA, KC_DOT,  HU_MINS,  KC_ALGR,  KC_NO,
+                                      KC_MPLY,   KC_LALT, KC_SPC,  MO(_FUNC),                             KC_DEL,  KC_ENT,  KC_BSPC,  KC_CPNL
     ),
 
     [_FUNC] = LAYOUT_4x7(
-        _______,  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,      KC_F6  , KC_F7  , KC_F8  ,  KC_F9  , KC_F10 ,  KC_F11 ,  KC_F12,
-        _______,  _______, _______, _______, _______, _______, _______,      _______, _______, KC_UP  ,  _______, _______,  KC_HOME,  KC_PGUP,
-        _______,  _______, _______, _______, _______, _______, _______,      _______, KC_LEFT, KC_DOWN,  KC_RGHT, _______,  KC_END ,  KC_PGDN,
-        _______,  _______, _______, _______, _______, _______, _______,      _______, _______, _______,  _______, _______,  _______,  _______,
-                                    _______, _______, _______, _______,      _______, KC_PSCR, _______,  _______
+        _______,  _______,            KC_F1  , KC_F2  , KC_F3  , KC_F4  ,              KC_F5  ,           KC_F6  , KC_F7  , KC_F8  ,  KC_F9  , KC_F10 ,  KC_F11 ,  KC_F12,
+        _______,  _______,            _______, _______, _______, _______,              _______,           _______, _______, KC_UP  ,  _______, _______,  KC_HOME,  KC_PGUP,
+        _______,  _______,            _______, _______, _______, _______,              _______,           _______, KC_LEFT, KC_DOWN,  KC_RGHT, _______,  KC_END ,  KC_PGDN,
+        _______,  _______,            _______, _______, _______, _______,              _______,           _______, _______, _______,  _______, _______,  _______,  _______,
+                                               _______, _______, _______,              _______,           _______, KC_PSCR, _______,  _______
     ),
 
     [_MOUS] = LAYOUT_4x7(
-        _______,  _______, _______, _______, _______, _______, _______,      _______, _______, _______,  _______, _______,  _______,  _______,
-        _______,  _______, _______, _______, _______, _______, _______,      _______, _______, MS_UP  ,  _______, _______,  _______,  _______,
-        _______,  _______, _______, _______, _______, _______, _______,      _______, MS_LEFT, MS_DOWN,  MS_RGHT, MS_WHLU,  _______,  _______,
-        _______,  _______, _______, _______, _______, _______, _______,      _______, _______, _______,  _______, MS_WHLD,  _______,  _______,
-                                    _______, _______, _______, _______,      MS_BTN3, MS_BTN1, MS_BTN2,  _______
+        _______,  _______,            _______, _______, _______, _______,              _______,           _______, _______, _______,  _______, _______,  _______,  _______,
+        _______,  _______,            _______, _______, _______, _______,              _______,           _______, _______, MS_UP  ,  _______, _______,  _______,  _______,
+        _______,  _______,            _______, _______, _______, _______,              _______,           _______, MS_LEFT, MS_DOWN,  MS_RGHT, MS_WHLU,  _______,  _______,
+        _______,  _______,            _______, _______, _______, _______,              _______,           _______, _______, _______,  _______, MS_WHLD,  _______,  _______,
+                                               _______, _______, _______,              _______,           MS_BTN3, MS_BTN1, MS_BTN2,  _______
     )
 
 
@@ -72,8 +72,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #ifdef OLED_ENABLE
 #include "demon.c"
 #endif
-
-// -- Probably some other stuff and then --
 
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
